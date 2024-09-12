a8c883aabf06b1248f1c5b0afb115354ab83a805
Ezequiel Salas <salaseze@gmail.com>
2024-09-11 21:58:02 -0300

    tags

--- app/assets/javascripts/discourse/app/controllers/artists.js
+++ app/assets/javascripts/discourse/app/controllers/artists.js
@@ -1,15 +1,19 @@
 import Controller from '@ember/controller';
-import { tracked, cached } from "@glimmer/tracking";
+import { tracked } from "@glimmer/tracking";
+import { computed } from '@ember/object';
+
 // import { getPromiseState } from '@warp-drive/ember';
 
 export default class ArtistController extends Controller {
   @tracked wikipediaData;
   @tracked reviewsData;
 
-  @cached
-  get request() {
-    let promise = fetch(this.args.url);
-
-    return getPromiseState(promise);
+  @computed('model.artist.tag.[]')
+  get formattedTags() {
+    console.log(this.model.artist.tag.artist);
+    return this.model.artist.tag.artist.map(tag => ({
+      count: tag.count,
+      label: tag.tag
+    }));
   }
 }
