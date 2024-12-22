b0513c7912ca4cda7535848bfdcc03093b0ca6cb
Ezequiel Salas <salaseze@gmail.com>
2024-12-22 16:30:46 -0300

    missing mb models associations wip

--- config/initializers/music_maniacs/active_musicbrainz.rb
+++ config/initializers/music_maniacs/active_musicbrainz.rb
@@ -1,2 +1,23 @@
 ActiveMusicbrainz::Model::Base.establish_connection(:music_brainz)
 ActiveMusicbrainz.init
+
+class ActiveMusicbrainz::Model::Artist
+  has_many :artist_tag, foreing_key: :artist
+end
+
+class ActiveMusicbrainz::Model::ArtistTag
+  belongs_to :artist, foreign_key: :artist
+  belongs_to :tag, foreign_key: :tag
+end
+
+class ActiveMusicbrainz::Model::Tag
+end
+
+
+class ActiveMusicbrainz::Model::LArtistUrl
+  belongs_to :link, foreign_key: :link
+end
+
+class ActiveMusicbrainz::Model::Link
+  belongs_to :link_type, foreign_key: :link_type
+end
