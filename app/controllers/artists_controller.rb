8636d05d50321d0683cc6d043d82da72ebc080ae
Ezequiel Salas <salaseze@gmail.com>
2024-12-30 20:13:25 -0300

    using activemusicbrainz artist :)

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 class ArtistsController < ApplicationController
   def show
-    # @mb_artist = MusicBrainz::Artist.find(params[:id])
-    # raise Discourse::NotFound unless @mb_artist
+    @mb_artist = ActiveMusicbrainz::Model::Artist.find_by!(gid: params[:id])
+    raise Discourse::NotFound unless @mb_artist
 
-    @mb_artist = Listenbrainz::Api.artist(params[:id])
-    @mb_artist.merge!(id: params[:id]) if @mb_artist
-    render json: @mb_artist.present? ? { artist: @mb_artist } : { error: "Artist not found" }
+    render json: MusicBrainz::Artist.new(@mb_artist)
   end
 
   # release_group: name, data, cover_art, listens?
