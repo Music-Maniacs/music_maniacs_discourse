94d28057aa94a38bd152c47493c1d191624269ad
Ezequiel Salas <salaseze@gmail.com>
2025-02-28 20:34:01 -0300

    chip component wip

---
+++ app/assets/javascripts/discourse/app/components/chip-component.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ChipComponent extends Component {
+  @tracked count = 1;
+  @tracked showControls = false;
+
+  get genre() {
+    return this.args.genre || 'post-rock';
+  }
+
+  @action
+  toggleControls() {
+    this.showControls = !this.showControls;
+  }
+
+  @action
+  incrementCount() {
+    this.count++;
+  }
+
+  @action
+  decrementCount() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+}
