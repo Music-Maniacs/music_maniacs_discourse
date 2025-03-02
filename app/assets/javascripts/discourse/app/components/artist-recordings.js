5bfd0bacc635879f30e15e018d6476224eb8ccb0
Ezequiel Salas <salaseze@gmail.com>
2025-03-02 15:11:19 -0300

    artist-recordings component name change

--- app/assets/javascripts/discourse/app/components/artist-recordings.js
+++ app/assets/javascripts/discourse/app/components/artist-recordings.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class ArtistRecordingsComponent extends Component {
+export default class ArtistRecordings extends Component {
   @service store;
   @tracked recordings = [];
 
